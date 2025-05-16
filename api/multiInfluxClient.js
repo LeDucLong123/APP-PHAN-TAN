d8a2d9e03c006bbb08aa6544cf77c38039ba6c8e
Lê Đức Long <longg56789@gmail.com>
2025-05-17 02:47:50 +0700

    fix influxdb

--- api/multiInfluxClient.js
+++ api/multiInfluxClient.js
@@ -3,19 +3,19 @@ const { InfluxDB } = require("@influxdata/influxdb-client");
 const configs = [
   {
     name: "node1",
-    url: "http://localhost:8087",
+    url: "http://influxdb-1:8087",
     token: "my-super-secret-token",
     org: "longgroup",
   },
   {
     name: "node2",
-    url: "http://localhost:8088",
+    url: "http://influxdb-2:8088",
     token: "my-super-secret-token",
     org: "longgroup",
   },
   {
     name: "node3",
-    url: "http://localhost:8089",
+    url: "http://influxdb-3:8089",
     token: "my-super-secret-token",
     org: "longgroup",
   },
