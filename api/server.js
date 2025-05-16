e53622b9e5876a99dc2dc7b63db74e41ccc478c3
Lê Đức Long <longg56789@gmail.com>
2025-05-16 23:25:13 +0700

    cors for api

--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { readAllWebRequests } = require("./query");
+const cors = require("cors");
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get("/api/data", async (req, res) => {
 
