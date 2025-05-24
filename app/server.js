415c09bb2f968d808ce0d75f3bbb9d9dcfed75de
Lê Đức Long <longg56789@gmail.com>
2025-05-24 14:58:56 +0700

    update interface

--- app/server.js
+++ app/server.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const requestLogger = require("./influxDB/requestLogger");
+const path = require("path");
 
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./src/views"));
+
+app.use(express.static(path.join(__dirname, "./src/views")));
+
 app.use(requestLogger);
 
 const PORT = process.env.PORT || "3000";
 const NAME = process.env.NAME || "server-name";
 
 app.get("/", (req, res) => {
-  res.send(
-    `
-      <title>App Server</title>
-      <h1>Chào Bạn!</h1>
-      ${NAME} is running on port ${PORT} - 
-      Write on http://${process.env.INFLUXDB}:${process.env.INFLUXDB_PORT}
-    `
-  );
+  res.render("welcome");
 });
 
 app.listen(PORT, () => {
